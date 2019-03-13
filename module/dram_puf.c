2bf196927519368fd82c5caf198a40576d09c4d9
soenke <me@eknoes.de>
2019-03-13 16:19:02 +0100

    Improve readability

--- module/dram_puf.c
+++ module/dram_puf.c
@@ -8,175 +8,148 @@
 #include <linux/slab.h>
 
 /************ Variable Declaration *********/
-#define INPUT_SIZE 1024*1024
+#define INPUT_SIZE 1024*1024 
+#define BASE_ADDR 0x10000000 // Physical Address for PUF generation
+#define PROCFS_NAME "dram_puf"
 
-#define INVERT_NEUMANN 0
+#define LOG_RAM 0
 
-#define PROCFS_MAX_SIZE		RAM_TESTSIZE
-#define PROCFS_NAME 		"neumann_puf"
-
-
-char * create_neumann(char *startAddr, int inputLen, int *outputSize);
-void read_ram(unsigned int * start, unsigned int offset);
+char * run_neumann(char *start_addr, int input_len, int *output_size, int inverted);
+void log_ram_content(unsigned int * start, unsigned int length);
 
 char *puf;
-int pufSize;
+int puf_size;
 
-static struct proc_dir_entry *ent;
+static struct proc_dir_entry *entry;
 
-static ssize_t mywrite(struct file *file, const char __user *ubuf,size_t count, loff_t *ppos) 
+static ssize_t read_puf(struct file *file, char __user *ubuf,size_t count, loff_t *ppos) 
 {
-	printk( KERN_DEBUG "write handler\n");
-	return -1;
-}
-
-static ssize_t myread(struct file *file, char __user *ubuf,size_t count, loff_t *ppos) 
-{
-    int total_len = 0;
-    printk( KERN_DEBUG " read from proc");
-
-    if(*ppos > 0 || count < pufSize)
+    if(*ppos > 0 || count < puf_size)
 		return 0;
     
-    if(copy_to_user(ubuf, puf, pufSize))
+    if(copy_to_user(ubuf, puf, puf_size))
         return -EFAULT;
 
-    total_len = pufSize;
+    *ppos = puf_size;
 
-    *ppos = total_len;
-
-    return total_len;
+    return puf_size;
 }
 
-static struct file_operations myops = 
+
+static struct file_operations proc_operations = 
 {
 	.owner = THIS_MODULE,
-	.read = myread,
-	.write = mywrite,
+	.read = read_puf,
 };
 
+int puf_init (void) {   
+    char *tmp_puf;
+    int size;
 
-int thread_init (void) {   
-    ent = proc_create(PROCFS_NAME, 0660, NULL, &myops);
+#ifdef LOG_RAM 
+    printk(KERN_NOTICE "Log RAM content");
+    log_ram_content(phys_to_virt(BASE_ADDR), INPUT_SIZE);
+#endif
     
-    puf = create_neumann(phys_to_virt(0x10000000), INPUT_SIZE, &pufSize);
+    entry = proc_create(PROCFS_NAME, 0660, NULL, &proc_operations);
     
-    printk(KERN_NOTICE "Generated PUF with size %i\n", pufSize);
-    
+    // Run inverted Neumann to remove 0101b bias
+    tmp_puf = run_neumann(phys_to_virt(BASE_ADDR), INPUT_SIZE, &size, true);
+    printk(KERN_NOTICE "Generated first PUF iteration with size %i\n", size);
+
+    // Run Neumann to remove 1111b bias
+    puf = run_neumann(tmp_puf, size, &puf_size, false);
+    printk(KERN_NOTICE "Generated final PUF iteration with size %i\n", puf_size);
+
+    kfree(tmp_puf);
+
     return 0;
 }
 
-#define LEFT_MASK 0x80
-#define LEFT_PAIR 0xc0
-#define RIGHT_PAIR 0x30
+#define LEFT_MASK 0x80 // leftmost bit
+#define LEFT_PAIR 0xc0 // leftmost bit pair
+#define RIGHT_PAIR 0x30 // second bit pair from the left
 
-char * create_neumann(char *startAddr, int inputLen, int *outputSize) {
-
+char * run_neumann(char *start_addr, int input_len, int *output_size, int inverted) {
     int i, bitpair, copied_bits, output_pos;
     unsigned char curr_pair;
-    char *outputAddr;
+    char *output_addr;
 
-    *outputSize = INPUT_SIZE / 4;
-    outputAddr = kmalloc(*outputSize, GFP_KERNEL);
+    // Expect reduction to quarter size
+    *output_size = INPUT_SIZE / 4;
+    output_addr = kmalloc(*output_size, GFP_KERNEL);
 
     output_pos = 0;
     copied_bits = 0;
 
-    for(i = 0; i < inputLen; i++) {
+    for(i = 0; i < input_len; i++) {
 
         for( bitpair = 0; bitpair < 4; bitpair++) {
-            // Copy current byte, shift to current pair, mask
-            curr_pair = *(startAddr + i);
+            // Copy current byte, shift to current pair, mask to get most left pair
+            curr_pair = *(start_addr + i);
             curr_pair = curr_pair << (bitpair * 2);
             curr_pair = curr_pair & LEFT_PAIR;
 
-#ifdef INVERT_NEUMANN
-            // Copy if not 00 or 01
-            if ( (curr_pair == 0xc0 || curr_pair == 0x00 ) ) {
-#else
-            if ( !(curr_pair == 0xc0 || curr_pair == 0x00 ) ) {
-#endif
-                *(outputAddr + output_pos) = *(outputAddr + output_pos) | (curr_pair >> 7);
 
-                // If 8 bits copied, move on, otherwise shift
+            // Actual Von Neumann Algorithm
+            // Copy if not 00 or 11, discard otherwise
+            if ( ( inverted && (curr_pair == 0xc0 || curr_pair == 0x00 ) ) || (!inverted && !(curr_pair == 0xc0 || curr_pair == 0x00 ) )) {
+                *(output_addr + output_pos) = *(output_addr + output_pos) | (curr_pair >> 7); // Copy bit to the rightmost output byte
+                
+                // If 8 bits copied, move on to next output byte, otherwise shift one bit
                 if(++copied_bits % 8 == 0) {
                     output_pos++;
-        
-                    if(*outputSize < (output_pos)) {
-                        *outputSize = output_pos + 24;
-                        outputAddr = krealloc(outputAddr, *outputSize, GFP_KERNEL);
+                    
+                    // If running out of output space, realloc
+                    if(*output_size < (output_pos)) {
+                        *output_size = output_pos + 24;
+                        output_addr = krealloc(output_addr, *output_size, GFP_KERNEL);
 
-                        if(outputAddr == NULL) {
+                        if(output_addr == NULL) {
                             printk("Could not realloc space");
                             return 0;
                         }
                     }
 
-                    *(outputAddr+output_pos) = 0x00;
+                    *(output_addr+output_pos) = 0x00;
                 } else {
-                    *(outputAddr + output_pos) = *(outputAddr + output_pos) << 1;
+                    *(output_addr + output_pos) = *(output_addr + output_pos) << 1;
                 }
             }
         }
     }
     
-    outputAddr = krealloc(outputAddr, copied_bits / 8, GFP_KERNEL);
-    pufSize = copied_bits / 8;
+    // Reallocate to discard unused output space
+    output_addr = krealloc(output_addr, copied_bits / 8, GFP_KERNEL);
+    puf_size = copied_bits / 8;
 
-    return outputAddr;
+    return output_addr;
 }
 
-int ram_to_buf(unsigned int * start, unsigned int testsize, char * buffer) {
+// Loop memory and log contents to kernel log
+void log_ram_content(unsigned int * start, unsigned int length) {
     unsigned int offset;
-    unsigned int *currentDRAMAddr;
-    unsigned int *startAddr;
-    unsigned int result;
-    unsigned int last_result;
-    unsigned int len = 0;
-        
-    startAddr = start;
-    for( offset = 0; offset < testsize; offset++ ) {         // Loop to read data from memory
-           currentDRAMAddr = ( start + offset );
-           result = *( currentDRAMAddr );                                          
-           if(last_result != result) {
-               last_result = result;
-               len += sprintf(buffer + len, "0x%x +%d: 0x%x\n", (unsigned int) startAddr, (unsigned int) (currentDRAMAddr - startAddr), result );
-               startAddr = currentDRAMAddr;
-           } 
-    }
-
-    return len;
-}
-
-
-void read_ram(unsigned int * start, unsigned int testsize) {
-    unsigned int offset;
-    unsigned int *currentDRAMAddr;
-    unsigned int *startAddr;
+    unsigned int *current_addr;
     unsigned int result;
 
-        
-    startAddr = start;
-    for( offset = 0; offset < testsize; offset++ ) {         // Loop to read data from memory
-           currentDRAMAddr = ( start + offset );
-           result = *( currentDRAMAddr );                                          
-               printk(KERN_NOTICE "0x%xx : 0x%x\n", (unsigned int) currentDRAMAddr, result );
-               startAddr = currentDRAMAddr;
-         
+    for( offset = 0; offset < length; offset++ ) {
+           current_addr = ( start + offset );
+           result = *( current_addr );                                          
+           printk(KERN_NOTICE "0x%xx : 0x%x\n", (unsigned int) current_addr, result );
     }
 }
 
-void thread_cleanup(void) {
+void puf_cleanup(void) {
     printk(KERN_ALERT "Module Removed\n");
-    proc_remove(ent); 
+    proc_remove(entry); 
     kfree(puf);
     return;
 }
 
 MODULE_LICENSE("GPL");   
-module_init(thread_init);
-module_exit(thread_cleanup);
+module_init(puf_init);
+module_exit(puf_cleanup);
 
-MODULE_AUTHOR("PANDABOARD Rowhammer-BASED DRAM PUF");
-MODULE_DESCRIPTION("Pandaboard Rowhammer-based DRAM PUF kernel module for changing DRAM refresh rate and reading/writing tp PUF memory locations");
+MODULE_AUTHOR("Soenke Huster, Fabian Hinz");
+MODULE_DESCRIPTION("Creates a PUF after initialization out of DRAM and applying transformation for high entropy");
 
